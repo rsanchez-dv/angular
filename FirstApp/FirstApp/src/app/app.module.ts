3ad71dc30d4cc505c5b2fffce5507f6ba8916190
Roberto Sanchez <rsanchez92@live.com>
2020-08-15 22:46:30 -0700

    Created a basic component

--- FirstApp/FirstApp/src/app/app.module.ts
+++ FirstApp/FirstApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+// Import the file
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Add the component
+    ServerComponent
   ],
   imports: [
     BrowserModule,
