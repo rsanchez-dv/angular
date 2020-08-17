82399e46abb845fa49a5da93de6cc60151d6e2dc
Roberto Sanchez <rsanchez92@live.com>
2020-08-16 20:41:25 -0700

    Created Component Templates
    Don't need a html file for all components

--- FirstApp/FirstApp/src/app/app.component.ts
+++ FirstApp/FirstApp/src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // Either template or templateURL
+  //templateURL: './app.component.html',
+  // Another way to create small components
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
